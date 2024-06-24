a3cd4e6fb9f28d70884e953b42cb925a0cc6a5a8
Willian Ribeiro Angelo <agfoccus@gmail.com>
2024-06-23 21:13:47 -0300

    test: refact tests

--- src/atomic/components/atoms/Button/index.tsx
+++ src/atomic/components/atoms/Button/index.tsx
@@ -4,17 +4,19 @@ import { TouchableOpacity } from 'react-native'
 import { Text } from '../Text'
 import { VariantProps } from 'tailwind-variants'
 import { ButtonVariants } from './Button.variants'
+import { assignTestId } from '@/src/utils/assignTestId'
 
 type VariantsProps = VariantProps<typeof ButtonVariants>
 export type ButtonProps = {
+  testID?: string
   onPress?: () => void
   children: string
 } & VariantsProps
 
-export const Button = ({ onPress, children, variant }: ButtonProps) => {
+export const Button = ({ onPress, children, testID = 'Button', variant }: ButtonProps) => {
   const { base, text } = ButtonVariants({ variant })
   return (
-    <TouchableOpacity onPress={onPress} style={[tailwind(base())]}>
+    <TouchableOpacity onPress={onPress} style={[tailwind(base())]} {...assignTestId('Button', testID)}>
       <Text style={[tailwind(text())]} numberOfLines={1}>
         {children}
       </Text>
